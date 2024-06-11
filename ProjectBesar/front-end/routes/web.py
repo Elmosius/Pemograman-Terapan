c48644c6a639c0a46dcd4c9c65069c24dfc26a60
Elmosius <2272008@maranatha.ac.id>
2024-06-11 16:31:32 +0700

    front-end
    Buat top-rated, benerin footer

--- ProjectBesar/front-end/routes/web.py
+++ ProjectBesar/front-end/routes/web.py
@@ -3,22 +3,26 @@ import requests
 
 website_bp = Blueprint('website', __name__)
 
-@website_bp.route('/')
-def index():
-    studio_response = requests.get('http://127.0.0.1:5000/studios')
-    studios = studio_response.json()
-
+@website_bp.app_context_processor
+def data():
     genres_response = requests.get('http://127.0.0.1:5000/genres')
     genres = genres_response.json()
+    
+    studio_response = requests.get('http://127.0.0.1:5000/studios')
+    studios = studio_response.json()
     
     anime_response = requests.get('http://127.0.0.1:5000/anime')
     animes = anime_response.json()
     
-    return render_template('website/index.html', studios=studios,genres=genres,animes=animes)
+    return dict(global_genres=genres, global_studios = studios, global_animes = animes)
+
+
+@website_bp.route('/')
+def index():
+    return render_template('website/index.html')
 
 @website_bp.route('/anime-detail/<int:id>')
-def anime_detail(id):
-
+def animeDetail(id):
     anime_response = requests.get(f'http://127.0.0.1:5000/anime/{id}')
     anime = anime_response.json()
     
@@ -27,23 +31,31 @@ def anime_detail(id):
 
 @website_bp.route('/enime-categories')
 def categories():
-
-    genres_response = requests.get('http://127.0.0.1:5000/genres')
-    genres = genres_response.json()
-    
-    anime_response = requests.get('http://127.0.0.1:5000/anime')
-    animes = anime_response.json()
-    
-    return render_template('website/categories.html',genres=genres, animes=animes)
+    return render_template('website/categories.html')
 
 @website_bp.route('/enime-categories/<int:id>')
 def animeCategory(id):
     genres_response = requests.get('http://127.0.0.1:5000/genres')
     genres = genres_response.json()
     
-    anime_response = requests.get(f'http://127.0.0.1:5000/anime?genre_id={id}')
+    anime_response = requests.get('http://127.0.0.1:5000/anime')
     animes = anime_response.json()
     
     selected_genre = next((genre for genre in genres if genre['id'] == id), None)
     
-    return render_template('website/anime-category.html', genres=genres, animes=animes, selected_genre=selected_genre)
+    if not selected_genre:
+        return render_template('website/anime-category.html', genres=genres, animes=[], selected_genre=None)
+    
+    filtered_animes = [anime for anime in animes if selected_genre['name'] in anime['genres']]
+    
+    return render_template('website/anime-category.html', genres=genres, animes=filtered_animes, selected_genre=selected_genre)
+
+@website_bp.route('/enime-top')
+def topAnime():
+    anime_response = requests.get('http://127.0.0.1:5000/anime')
+    animes = anime_response.json()
+    
+    top_animes = [anime for anime in animes if anime['rating'] > 8]
+    
+    return render_template('website/top-anime.html', animes=top_animes)
+
